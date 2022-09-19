246c843767be552fa7d23289a6fe14a9fe52b1e0
keepdown3 <113839828+keepdown3@users.noreply.github.com>
2022-09-19 14:28:05 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,17 +43,17 @@ global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['12512077721']
+global.ownertag = 'keepdown yt' //ur tag number
+global.botnumber = '12512077721' //Add your Bot Number For Log
+global.botname = 'KEEPDOWN YT' //ur bot name
+global.ig = "https://youtube.com/channel/UCRObCIvc52_68qhqfgUXRMA" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.instagram.com/h_aitakka?r=nametag" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
+global.packname = "keepdown yt" //ur sticker watermark packname
 global.author = "DARK ALPHA" //ur sticker watermark author
 global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
